66353794d7d07b863e90424ca512540ee20e4d95
been2 <nrjs3k@gmail.com>
2021-06-07 06:27:03 +0900

    지원이 코드 소생

--- src/Project/controller/electionInfoAddController.java
+++ src/Project/controller/electionInfoAddController.java
@@ -1,17 +1,22 @@
-package Project.controller;
+package client.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class electionInfoAddController implements Initializable {
 	@FXML private ImageView election1;
@@ -45,7 +50,6 @@ public class electionInfoAddController implements Initializable {
       	     };
         });
     }
-
     
     public void changeElection1()
     {
@@ -78,5 +82,71 @@ public class electionInfoAddController implements Initializable {
     	electionInfo.getChildren().add(main);
     	}
     	catch(Exception e) { System.out.println("error");}
+    }
+    
+    public void setElection1VoteDay(String voteDay_in)
+    {
+    	election1VoteDay.setText(voteDay_in);
+    }
+    
+    public void setElection2VoteDay(String voteDay_in)
+    {
+    	election2VoteDay.setText(voteDay_in);
+    }
+    
+    public void setElection3VoteDay(String voteDay_in)
+    {
+    	election3VoteDay.setText(voteDay_in);
+    }
+    
+    public void setElection1Mark(String election_category)
+    {
+    	try
+    	{
+        	Thread thread = new Thread() {
+        		public void run() {
+        	    	election1Mark.setImage(new Image(getClass().getResourceAsStream("../img/"+ election_category +"_사진.jpg")));
+        		}
+        	};
+        	thread.setDaemon(true);
+        	thread.start();
+    	}
+    	catch(Exception e) {
+    		election1Mark.setImage(new Image(getClass().getResourceAsStream("../img/흰색_마크.png")));
+    		System.out.println("error");}
+    }
+    
+    public void setElection2Mark(String election_category)
+    {
+    	try
+    	{
+        	Thread thread = new Thread() {
+        		public void run() {
+        	    	election2Mark.setImage(new Image(getClass().getResourceAsStream("../img/"+ election_category +"_사진.jpg")));
+        		}
+        	};
+        	thread.setDaemon(true);
+        	thread.start();
+    	}
+    	catch(Exception e) {
+    		election2Mark.setImage(new Image(getClass().getResourceAsStream("../img/흰색_마크.png")));
+    		System.out.println("error");}
+    }
+    
+    public void setElection3Mark(String election_category)
+    {
+    	try
+    	{
+        	Thread thread = new Thread() {
+        		public void run() {
+        	    	election3Mark.setImage(new Image(getClass().getResourceAsStream("../img/"+ election_category +"_사진.jpg")));
+        		}
+        	};
+        	thread.setDaemon(true);
+        	thread.start();
+    	}
+    	catch(Exception e) {
+    		election3Mark.setImage(new Image(getClass().getResourceAsStream("../img/흰색_마크.png")));
+    		System.out.println("error");}
     }
 }
