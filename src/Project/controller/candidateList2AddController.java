f4e89f3869d843be5087b7df97b54f216851385a
lsminkk <lsminkor@gmail.com>
2021-06-07 10:32:24 +0900

    ㅜㅠㅜㅠㅜㅠㅜㅠㅜㅠ

--- src/Project/controller/candidateList2AddController.java
+++ src/Project/controller/candidateList2AddController.java
@@ -99,7 +99,11 @@ public class candidateList2AddController implements Initializable {
 						oos.flush();
 						ArrayList<CandidateDTO> candidateList = new ArrayList<>();
 						candidateList = (ArrayList<CandidateDTO>) ois.readObject();
-						System.out.println(candidateList.get(0).getName());
+						setCandidate1(candidateList.get(0).getName());
+						setCandidate2(candidateList.get(1).getName());
+						setCandidate3(candidateList.get(2).getName());
+						setCandidate4(candidateList.get(3).getName());
+						setCandidate5(candidateList.get(4).getName());
 					} catch (IOException | ClassNotFoundException ioException) {
 						ioException.printStackTrace();
 					}
@@ -112,37 +116,37 @@ public class candidateList2AddController implements Initializable {
 		}
           candidate1.setOnMouseClicked(new EventHandler<MouseEvent>() {
       	     public void handle(MouseEvent event) {
-      	    	 changeCandidateInfo();
+      	    	 changeCandidateInfo(candidate1Name.getText());
       	     };
         });
           
           candidate2.setOnMouseClicked(new EventHandler<MouseEvent>() {
       	     public void handle(MouseEvent event) {
-      	    	 changeCandidateInfo();
+      	    	 changeCandidateInfo(candidate1Name.getText());
       	     };
         });
 
           candidate3.setOnMouseClicked(new EventHandler<MouseEvent>() {
       	     public void handle(MouseEvent event) {
-      	    	 changeCandidateInfo();
+      	    	 changeCandidateInfo(candidate1Name.getText());
       	     };
         });
           
 
           candidate4.setOnMouseClicked(new EventHandler<MouseEvent>() {
       	     public void handle(MouseEvent event) {
-      	    	 changeCandidateInfo();
+      	    	 changeCandidateInfo(candidate1Name.getText());
       	     };
         });
           
 
           candidate5.setOnMouseClicked(new EventHandler<MouseEvent>() {
       	     public void handle(MouseEvent event) {
-      	    	 changeCandidateInfo();
+      	    	 changeCandidateInfo(candidate1Name.getText());
       	     };
         });
     }
-    public void changeCandidateInfo()
+    public void changeCandidateInfo(String candidateName)
     {
     	try
     	{
@@ -152,6 +156,7 @@ public class candidateList2AddController implements Initializable {
         		public void run() {
     	    		Stage primaryStage = (Stage) candidate1.getScene().getWindow();
         			Platform.runLater(()->{primaryStage.setScene(scene);});
+
         		}
         	};
         	thread.setDaemon(true);
@@ -197,16 +202,11 @@ public class candidateList2AddController implements Initializable {
     {
     	try
     	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate1.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
+			candidate1Name.setText(candidate_name);
+			candidate1.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+
     	}
     	catch(Exception e) {
-    		candidate1.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
     		System.out.println("error");}
     }
     
@@ -214,16 +214,10 @@ public class candidateList2AddController implements Initializable {
     {
     	try
     	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate2.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
+			candidate2Name.setText(candidate_name);
+    		candidate2.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
     	}
     	catch(Exception e) {
-    		candidate2.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
     		System.out.println("error");}
     }
     
@@ -231,16 +225,10 @@ public class candidateList2AddController implements Initializable {
     {
     	try
     	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate3.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
+			candidate3Name.setText(candidate_name);
+    		candidate3.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
     	}
     	catch(Exception e) {
-    		candidate3.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
     		System.out.println("error");}
     }
     
@@ -248,16 +236,10 @@ public class candidateList2AddController implements Initializable {
     {
     	try
     	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate4.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
+			candidate4Name.setText(candidate_name);
+    		candidate4.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
     	}
     	catch(Exception e) {
-    		candidate4.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
     		System.out.println("error");}
     }
     
@@ -265,187 +247,119 @@ public class candidateList2AddController implements Initializable {
     {
     	try
     	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate5.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
+			candidate5Name.setText(candidate_name);
+    		candidate5.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
     	}
     	catch(Exception e) {
-    		candidate5.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
     		System.out.println("error");}
     }
     
-    public void setCandidate6(String candidate_name)
-    {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate6.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate6.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
-    }
+    public void setCandidate6(String candidate_name) {
+		try {
+			candidate6Name.setText(candidate_name);
+			candidate6.setImage(new Image(getClass().getResourceAsStream("../img/" + candidate_name + "_사진.jpg")));
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+	}
     
     public void setCandidate7(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate7.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate7.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate7Name.setText(candidate_name);
+			candidate7.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate8(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate8.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate8.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate8Name.setText(candidate_name);
+			candidate8.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate9(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate9.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate9.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate9Name.setText(candidate_name);
+			candidate9.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate10(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate10.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate10.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate10Name.setText(candidate_name);
+			candidate10.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate11(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate11.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate11.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate11Name.setText(candidate_name);
+			candidate11.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate12(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate12.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate12.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate12Name.setText(candidate_name);
+			candidate12.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate13(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate13.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate13.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate13Name.setText(candidate_name);
+			candidate13.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate14(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate14.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate14.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate14Name.setText(candidate_name);
+			candidate14.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate15(String candidate_name)
     {
-    	try
-    	{
-        	Thread thread = new Thread() {
-        		public void run() {
-        	    	candidate15.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
-        		}
-        	};
-        	thread.setDaemon(true);
-        	thread.start();
-    	}
-    	catch(Exception e) {
-    		candidate15.setImage(new Image(getClass().getResourceAsStream("../img/기본이미지.png")));
-    		System.out.println("error");}
+		try
+		{
+			candidate15Name.setText(candidate_name);
+			candidate15.setImage(new Image(getClass().getResourceAsStream("../img/"+ candidate_name +"_사진.jpg")));
+		}
+		catch(Exception e) {
+			System.out.println("error");}
     }
     
     public void setCandidate1Name(String candidate_number_name)
